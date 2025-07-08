117db447b74ceb05533b89f2a9ed6ed2291861b8
Harshal Adhav <harshal.adhav@amd.com>
2025-07-08 04:07:21 -0600

    [ZENDNN] Graph pass to add weight prepacking
    -- Register graph replacement patterns to add
       weight prepack op into aot inductor graph
    -- Enable weight prepack optimization through
       zendnn's optimize api when inductor config
       for weight_prepack is True with inductor's
       freezing path
    -- Add test to validate accuracy when prepack
       op is inserted, as well to validate weight
       prepack op is inserted into the graph
    
    Change-Id: Ib4722e816cf19d6dadddc0a060067eca698d1063

--- torch/_inductor/fx_passes/zendnn_optimize.py
+++ torch/_inductor/fx_passes/zendnn_optimize.py
@@ -1,12 +1,17 @@
 # mypy: allow-untyped-defs
+from torch._inductor import config
 from torch._inductor.pattern_matcher import stable_topological_sort
 
+from .zendnn_custom_passes import add_zendnn_weight_prepack_ops
 from .zendnn_op_replacements import replace_with_zendnn_ops
 
 
 def optimize(graph):
     # replace aten ops with zendnn ops
     opt_graph = replace_with_zendnn_ops(graph)
+    if config.cpp.weight_prepack:
+        # replace zendnn ops with zendnn custom passes
+        opt_graph = add_zendnn_weight_prepack_ops(opt_graph)
     # topological-sort, lint and recompile
     stable_topological_sort(opt_graph.graph)
     opt_graph.graph.lint()
